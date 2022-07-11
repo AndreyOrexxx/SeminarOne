cb54640654038003550c6c147b532bdb09279454
andrey_orexxx <KO.orexxx@yandex.ru>
2022-07-11 23:27:44 +0300

    Задача 4 решена

---
+++ Tesk4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4
+// Напишите программу, которая принимает на ввод три числа и выдает максимальное из них.
+
+// Я понимаю задачу так :
+// Числа целые, генерируются автоматом из диапазона (10, 20)
+// Результат выводится в терминал, как показанно в примере.
+
+int x = new Random().Next(10, 20);
+int y = new Random().Next(10, 20);
+int z = new Random().Next(10, 20);
+int max = x;
+
+if (x > max) max = x;
+if (y > max) max = y;
+if (z > max) max = z;
+
+    Console.WriteLine(x);
+    Console.WriteLine(y);
+    Console.WriteLine(z);
+    Console.WriteLine(max);
+
